a1f54e3c078451169fe40dda03d4f6e9c7e054fc
muhamad jamil <muhamadjamil668@gmail.com>
2018-05-06 11:24:40 +0700

    semua

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+class HomeController extends Controller
+{
+  public function index()
+  {
+    return $this->view('home');
+  }
+
+  public function getusers()
+  {
+  	$users = $this->model('User')->index();
+  	return $this->view('users', ['users' => $users]);
+  }
+
+}
+
+
